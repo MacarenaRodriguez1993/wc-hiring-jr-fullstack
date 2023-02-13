5a10142513552a4c809b8839ef184bfdf6a8648c
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-02-13 20:02:37 -0300

    Question 1 is right

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ However, if you have any questions, please send us an email
 to support@webcat.app with the subject "Jr Fullstack Test Questions"
 */
 
-import $t from './libs/test.js'
+import $t from "./libs/test.js";
 
 /*
 1. Data manipulation:
@@ -33,12 +33,54 @@ import $t from './libs/test.js'
   Hint: Use native array methods as well as
     Lodash(https://lodash.com/docs) modules.
 */
-import _ from 'lodash'
-const source = $t.source(1)
+import _ from "lodash";
+const source = $t.source(1);
 $t.answer(1, async () => {
-  // Your code goes here
-  return 
-})
+  let Target_Data = {};
+  let expense = 0;
+  let income = 0;
+  let balance = 0;
+  let Restaurants = 0;
+  let Income = 0;
+  let Groceries = 0;
+  let Rent = 0;
+  /* encuentro gastos ganancias y balance*/
+  _.partition(source, function (o) {
+    /*pregunto si es ingreso o gasto  */
+    if (o.type === "income") {
+      /* verifico por categoria y sumo ingresos respectivos por categoria*/
+      if (o.category === "Restaurants") Restaurants = Restaurants + o.amount;
+      if (o.category === "Income") Income = Income + o.amount;
+      if (o.category === "Groceries") Groceries = Groceries + o.amount;
+      if (o.category === "Rent") Rent = Rent + o.amount;
+      /**  coloco el total de las ganancias */
+      income = income + o.amount;
+    } else if (o.type === "expense") {
+      /* verifico por categoria y resto gastos respectivos por categoria*/
+      if (o.category === "Restaurants") Restaurants = Restaurants - o.amount;
+      if (o.category === "Income") Income = Income - o.amount;
+      if (o.category === "Groceries") Groceries = Groceries - o.amount;
+      if (o.category === "Rent") Rent = Rent - o.amount;
+      /**  coloco el total de las gastos */
+      expense = expense + o.amount;
+    }
+  });
+  /** hago el balance entre ingresos y gastos */
+  balance = income - expense;
+  Target_Data = {
+    balance: balance,
+    income: income,
+    expenses: expense,
+    byCategories: {
+      Restaurants: Restaurants,
+      Income: Income,
+      Groceries: Groceries,
+      Rent: Rent,
+    },
+  };
+  console.log(Target_Data);
+  return Target_Data;
+});
 
 /*
 2. Asynchronous programming: 
@@ -47,11 +89,11 @@ $t.answer(1, async () => {
   3. Finally, return the list of resulting texts as an array.
     
 */
-const $source = $t.source(2)
+const $source = $t.source(2);
 $t.answer(2, async () => {
-    // Your code goes here:
-    // 1. Get ids: $source.getIds()
-    // 2. Get text for every id: $source.getText(id)
-    // 3. Return array of texts
-    return 
-})
+  // Your code goes here:
+  // 1. Get ids: $source.getIds()
+  // 2. Get text for every id: $source.getText(id)
+  // 3. Return array of texts
+  return;
+});
